481465703d7dd5e7139c1e522f256fb1422b60e0
Artyom Bubulchuk <20347829@myclyde.ac.uk>
2023-01-25 13:18:13 +0000

    Initial commit

---
+++ src/StudentGrades.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StudentGrades {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
